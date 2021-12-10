4793affc15d9f0fd2ffcb2f46c16035f38751fb5
Dareba <95076215+Dareba@users.noreply.github.com>
2021-12-10 11:21:01 +0100

    Komituji kod s testy

---
+++ test_main.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_add(self):
+        self.fail()
+
+    def test_subtract(self):
+        self.fail()
+
+    def test_multiply(self):
+        self.fail()
+
+    def test_divide(self):
+        self.fail()
+
+    def test_factorial(self):
+        self.fail()
