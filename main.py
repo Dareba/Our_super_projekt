a35e8d44b432bca7bee94ed0a9dcde5506d9117f
Dareba <95076215+Dareba@users.noreply.github.com>
2021-12-10 09:53:49 +0100

    komituji 'upraveny' kod

--- main.py
+++ main.py
@@ -22,17 +22,26 @@ def divide(x, y):
     return x / y
 
 
+
+
 # Tady definujeme fuknci faktorial
 def factorial(x):
     return math.factorial(x)
 
 
 print("Vyber si operaci:")
+
 print("1.Scitani")
+
 print("2.Odcitani")
+
 print("3.Nasobeni")
+
 print("4.Deleni")
+
+
 print("5.Faktorial")
+
 while True:
     # take input from the user
     choice = input("Enter choice(1/2/3/4/5): ")
@@ -45,9 +54,9 @@ while True:
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
 
+
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
-
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
@@ -59,12 +68,18 @@ while True:
         if num3 < 0:
             print("Nedefinovano, zdaej kladne cislo!")
 
+
         elif choice == '5':
+
+
             print(num3, '!', '=', factorial(num3))
 
         next_calculation = input("Let's do next calculation? (yes/no): ")
+
+
         if next_calculation == "no":
             break
+
 
     else:
         print("Invalid Input")
