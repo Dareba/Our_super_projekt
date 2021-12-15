9e7c2b4e8dc4fb143852441fb4e6ff2b9cd194b0
tom-b1995 <78988374+tom-b1995@users.noreply.github.com>
2021-12-15 14:34:06 +0100

    Unittests added/Pridany unittesty

---
+++ test_main.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from main import *
+
+class Test(TestCase):
+    def test_add(self):
+        self.fail()
+
+    def test_subtract(self):
+        self.fail()
+
+    def test_multiply(self):
+        self.fail()
+
+    def test_divide(self):
+        self.fail()
+
+    def test_factorial(self):
+        self.fail()
+
+#Vzor testu
+#class MyTestCase(unittest.TestCase):
+    #def test_something(self):
+        #self.assertEqual(True, False)  # add assertion here
+
+#if __name__ == '__main__':
+    #unittest.main()
