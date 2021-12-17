bb766ccd1140eb639c4be2873be703a38e2882f5
Dareba <95076215+Dareba@users.noreply.github.com>
2021-12-17 09:18:05 +0100

    commit kodu uprava

--- main.py
+++ main.py
@@ -29,60 +29,60 @@ def divide(x, y):
 def factorial(x):
     return math.factorial(x)
 
+if __name__ == '__main__':
+    print("Vyber si operaci:")
 
-print("Vyber si operaci:")
+    print("1.Scitani")
 
-print("1.Scitani")
+    print("2.Odcitani")
 
-print("2.Odcitani")
+    print("3.Nasobeni")
 
-print("3.Nasobeni")
+    print("4.Deleni")
 
-print("4.Deleni")
+    print("5.Faktorial")
 
-print("5.Faktorial")
-
-while True:
+    while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4/5): ")
+        choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Vloz prvni cislo: "))
-        num2 = float(input("Vloz druhe cislo: "))
+        if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Vloz prvni cislo: "))
+            num2 = float(input("Vloz druhe cislo: "))
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            if choice == '1':
+                print(num1, "+", num2, "=", add(num1, num2))
 
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            elif choice == '2':
+                print(num1, "-", num2, "=", subtract(num1, num2))
+            elif choice == '3':
+                print(num1, "*", num2, "=", multiply(num1, num2))
 
-        if choice in ('4'):
+            if choice in ('4'):
 
-            if num2 == 0:
-                print("Nemuzes delit nulou, zkus to znovu! xd")
+                if num2 == 0:
+                    print("Nemuzes delit nulou, zkus to znovu! xd")
+                else:
+                    print(num1, "/", num2, "=", divide(num1, num2))
+
+        if choice in ('5'):
+
+            num3 = int(input("Vloz  cislo: "))
+            if num3 < 0:
+                print("Nedefinovano, musis zadat kladne cislo!")
+
+
             else:
-                print(num1, "/", num2, "=", divide(num1, num2))
 
-    if choice in ('5'):
+                print(num3, '!', '=', factorial(num3))
 
-        num3 = int(input("Vloz  cislo: "))
-        if num3 < 0:
-            print("Nedefinovano, musis zadat kladne cislo!")
+            next_calculation = input("Chces pokracovat v kalkulaci?? (y/n): ")
+
+            if next_calculation == "n":
+                break
 
 
         else:
-
-            print(num3, '!', '=', factorial(num3))
-
-        next_calculation = input("Chces pokracovat v kalkulaci?? (y/n): ")
-
-        if next_calculation == "n":
-            break
-
-
-    else:
-        print("Invalid Input")
+            print("Invalid Input")
